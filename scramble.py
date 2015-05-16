3be7b03727124f47c9e61fceeeff6c0e05426f49
Rudolf Horvath <rudolf.official@gmail.com>
2015-05-16 19:27:36 +0200

    Creating unit tests. Better string representation support.

--- scramble.py
+++ scramble.py
@@ -1,14 +1,23 @@
 __author__ = 'Rudolf Horvath'
 
+import doctest
+
 
 class TextBlock:
+    """
+    This class represents a text sequence. It can have a given type like *word*, *special*,
+    and it can be scrambled.
+    """
     textTypes = {"word":0, "special":1}
 
     def scramble(self):
         """
+        This function leaves the first and last character in this TextBlock in place,
+        but the remaining middle characters will be written back in a reverse order.
+
         >>> test = TextBlock("alma", "word")
         >>> test.scramble()
-        >>> print test.text
+        >>> print test
         amla
         """
         block = self.text
@@ -28,7 +37,7 @@ class TextBlock:
         else:
             self.text = block
 
-    def toString(self):
+    def __str__(self):
         return self.text
 
     def __init__(self, text, type):
@@ -38,6 +47,16 @@ class TextBlock:
 
 
 class TextSplitter:
+    """
+    This class splits a given text sequence into words and other remaining character sequences.
+    It can be given, what characters don't represent words.
+    The word blocks can be scrambled, to get a special text.
+
+    >>> test = TextSplitter("According to research at Cambridge University, it doesn't matter in what order the letters in a word are, the only important thing is that the first and last letter be at the right place. The rest can be a total mess and you can still read it without a problem. This is because the human mind does not read every letter by itself, but the word as a whole.")
+    >>> test.scrambleWords()
+    >>> print test
+    Anidroccg to rcraeseh at Cgdirbmae Utisreviny, it dseon't mettar in waht oedrr the lrettes in a wrod are, the olny inatropmt tnihg is taht the fsrit and lsat letter be at the rhgit pcale. The rset can be a tatol mses and you can slitl raed it wuohtit a pelborm. Tihs is bsuacee the hamun mnid deos not raed erevy letter by ilestf, but the wrod as a wlohe.
+    """
 
     def scrambleWords(self):
         for block in self.blocks:
@@ -68,9 +87,13 @@ class TextSplitter:
         newBlock = TextBlock(tmpBlock, lastBlockType)
         self.blocks.append(newBlock)
 
-    def toString(self):
+    def __str__(self):
         res = ""
         for block in self.blocks:
-            res += block.toString()
+            res += str(block)
         return res
 
+
+if __name__ == "__main__":
+    doctest.testmod(verbose=True)
+    #doctest.testfile("scramble.test", verbose=True)
